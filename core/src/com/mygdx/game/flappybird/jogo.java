fabe521d67079eb75d0770f61c74a6082489bef1
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-05-06 20:25:18 -0300

    Flappy Bird Aula 06/05/2021

--- core/src/com/mygdx/game/flappybird/jogo.java
+++ core/src/com/mygdx/game/flappybird/jogo.java
@@ -8,46 +8,68 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class jogo extends ApplicationAdapter {
 
-	//criação das variaveis
-	//para que o jogo entenda a imagem que será colocada
+
 	private SpriteBatch batch;
-	private Texture passaro;
+	private Texture [] passaros;	//criação das variaveis para que o jogo entenda a imagem que será colocada
 	private Texture fundo;
 
-	//movimentam o passaro
-	private int movimentaY = 0;
+
+	private int movimentaY = 0;	//movimentam o passaro
 	private int movimentaX = 0;
 
 
-	//deixam o game responsivo
-	private float larguraDispositivo;
+
+	private float larguraDispositivo;	//deixam o game responsivo
 	private float alturaDispositivo;
+
+
+	private float variacao = 0;  // criação do passaro
+	private float gravidade = 0; // gravidade para fazer o passaro cair
+	private float posicaoInicialVerticalPassaro = 0; // posição inicial do passaro na tela
 
 	@Override
 	public void create () {
-		//criação dos objetos para que possam ser gerados na tela
+
 		batch = new SpriteBatch();
-		passaro = new Texture("passaro1.png");
+		passaros = new Texture[3];
+		passaros [0] = new Texture("passaro1.png");
+		passaros [1] = new Texture("passaro2.png");	//sprites para fazer a animação do passarinho
+		passaros [2] = new Texture("passaro3.png");
 		fundo = new Texture("fundo.png");
-		//identifica a largura da tela do dispositivo
-		larguraDispositivo = Gdx.graphics.getWidth();
-		//identifica a altura da tela do dispositivo
-		alturaDispositivo = Gdx.graphics.getHeight();
+
+
+		larguraDispositivo = Gdx.graphics.getWidth();	//identifica a largura da tela do dispositivo
+
+		alturaDispositivo = Gdx.graphics.getHeight();	//identifica a altura da tela do dispositivo
+
+		posicaoInicialVerticalPassaro = alturaDispositivo / 2;	//altura inicial do passaro de acordo com a tela do dispositivo
 	}
 
 	@Override
 	public void render () {
 
 		batch.begin();
-		//adequa a tela do game para cada dispositivo
-		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-		//renderiza o passaro na tela
-		batch.draw(passaro, 50, 50, movimentaX, movimentaY);
 
+		if(variacao > 3)
+			variacao = 0;
+		boolean toqueTela = Gdx.input.justTouched();
+		if (Gdx.input.justTouched()){
+			gravidade = -25;
+		}
+		if(posicaoInicialVerticalPassaro > 0 || toqueTela)
+			posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
+
+		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+		batch.draw(passaros[(int) variacao], 30, posicaoInicialVerticalPassaro);
+
+
+		variacao += Gdx.graphics.getDeltaTime() * 10; //altera a velocidade das asas do passaro
+
+		gravidade++;
 		movimentaY++;
 		movimentaX++;
-
 		batch.end();
+
 	}
 	
 	@Override
