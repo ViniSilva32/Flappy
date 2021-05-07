8f52cc3b0c3485360e4608cfcdd51879c3570964
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-05-07 20:50:25 -0300

    Aula 07/05/2021 - Flappy Bird

--- core/src/com/mygdx/game/flappybird/jogo.java
+++ core/src/com/mygdx/game/flappybird/jogo.java
@@ -6,72 +6,109 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.Random;
+
 public class jogo extends ApplicationAdapter {
 
 
+
+	private int movimentaX = 0;
+	private int gravidade = 0; 									// gravidade para fazer o passaro cair
+	//private int movimentaXcano = 0;
+
 	private SpriteBatch batch;
-	private Texture [] passaros;	//criação das variaveis para que o jogo entenda a imagem que será colocada
+	private Texture [] passaros;								//criação das variaveis para que o jogo entenda a imagem que será colocada
+	private Texture canoTopo;
+	private Texture canoBaixo;
 	private Texture fundo;
 
 
-	private int movimentaY = 0;	//movimentam o passaro
-	private int movimentaX = 0;
-
-
-
-	private float larguraDispositivo;	//deixam o game responsivo
+	private float larguraDispositivo;							//deixam o game responsivo
 	private float alturaDispositivo;
+	private float variacao = 0;  								// criação do passaro
+	private float posicaoInicialVerticalPassaro = 0;
+	private float posicaoCanoHorizonatal;
+	private float espacoEntreCanos;
+	private float posicaoCanoVertical;
 
-
-	private float variacao = 0;  // criação do passaro
-	private float gravidade = 0; // gravidade para fazer o passaro cair
-	private float posicaoInicialVerticalPassaro = 0; // posição inicial do passaro na tela
+	private Random random;
 
 	@Override
 	public void create () {
 
+		inicializarTexturas();									//criação dos metodos
+		InicializarObjetos();
+
+	}
+
+	private void InicializarObjetos() {
+
+
+		larguraDispositivo = Gdx.graphics.getWidth();			//identifica a largura da tela do dispositivo
+		alturaDispositivo = Gdx.graphics.getHeight();			//identifica a altura da tela do dispositivo
+		posicaoInicialVerticalPassaro = alturaDispositivo / 2;	//altura inicial do passaro de acordo com a tela do dispositivo
+		posicaoCanoHorizonatal = larguraDispositivo;
+		espacoEntreCanos = 150;
+	}
+
+	private void inicializarTexturas() {
 		batch = new SpriteBatch();
+		random = new Random();
+
 		passaros = new Texture[3];
 		passaros [0] = new Texture("passaro1.png");
 		passaros [1] = new Texture("passaro2.png");	//sprites para fazer a animação do passarinho
 		passaros [2] = new Texture("passaro3.png");
+
 		fundo = new Texture("fundo.png");
-
-
-		larguraDispositivo = Gdx.graphics.getWidth();	//identifica a largura da tela do dispositivo
-
-		alturaDispositivo = Gdx.graphics.getHeight();	//identifica a altura da tela do dispositivo
-
-		posicaoInicialVerticalPassaro = alturaDispositivo / 2;	//altura inicial do passaro de acordo com a tela do dispositivo
+		canoBaixo = new Texture("cano_baixo_maior.png");
+		canoTopo = new Texture("cano_topo_maior.png");
 	}
 
 	@Override
 	public void render () {
 
-		batch.begin();
+		verificarEstadoJogo(); //
+		desenharTexturas();   //
 
-		if(variacao > 3)
-			variacao = 0;
-		boolean toqueTela = Gdx.input.justTouched();
+
+	}
+
+	private void verificarEstadoJogo() {
+
+		posicaoCanoHorizonatal-= Gdx.graphics.getDeltaTime() * 200;
+		if(posicaoCanoHorizonatal < - canoBaixo.getWidth()){
+			posicaoCanoHorizonatal = larguraDispositivo;
+			posicaoCanoHorizonatal = random.nextInt(400) -200;
+		}
+
+		boolean toqueTela = Gdx.input.justTouched(); 			// o passaro interage com o toque na tela
 		if (Gdx.input.justTouched()){
 			gravidade = -25;
 		}
 		if(posicaoInicialVerticalPassaro > 0 || toqueTela)
 			posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
 
-		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-		batch.draw(passaros[(int) variacao], 30, posicaoInicialVerticalPassaro);
+		variacao += Gdx.graphics.getDeltaTime() * 10; 			//altera a velocidade das asas do passaro
+		if(variacao > 3)
+			variacao = 0;
 
-
-		variacao += Gdx.graphics.getDeltaTime() * 10; //altera a velocidade das asas do passaro
-
-		gravidade++;
-		movimentaY++;
+		gravidade++;								//adiciona gravidade para fazer o passaro cair
 		movimentaX++;
+	}
+
+	private void desenharTexturas () {
+
+		batch.begin();
+		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+		batch.draw(passaros[(int) variacao], movimentaX,  posicaoInicialVerticalPassaro);
+		batch.draw(canoBaixo, posicaoCanoHorizonatal, alturaDispositivo /2 - canoBaixo.getHeight() - espacoEntreCanos /2 + posicaoCanoHorizonatal);
+		batch.draw(canoTopo,posicaoCanoHorizonatal,alturaDispositivo/2 + espacoEntreCanos /2 + posicaoCanoVertical);
+
 		batch.end();
 
 	}
-	
+
 	@Override
 	public void dispose () {
 
